439169412688d0e110c8cfc8295031dfe7d220c0
MoAhmedX <137880702+mo-ahmedx@users.noreply.github.com>
2025-06-18 21:48:17 +0300

    Update main.js

--- mo-ahmedx-portfolio/public/main.js
+++ mo-ahmedx-portfolio/public/main.js
@@ -1,1 +1,9 @@
-
+function showSidebar() {
+      document.querySelector('.side-bar').style.display = 'flex';
+    }
+function hideSidebar() {
+      document.querySelector('.side-bar').style.display = 'none';
+    }
+    document.querySelectorAll('.side-bar a').forEach(link => {
+      link.addEventListener('click', hideSidebar);
+    });
